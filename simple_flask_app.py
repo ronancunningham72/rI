0359e2daa8b96eecce20c309c594daca1b720d9b
Ronan Cunningham <ronan20@btinternet.com>
2018-04-29 21:47:23 +0100

    Add files via upload
    Simple Flask App

---
+++ simple_flask_app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import os
+import json
+
+parser = reqparse.RequestParser()
+parser.add_argument('host', type=str, required=True, action='append', 
+                     help='Enter each host as a cli arg with -d')
+parser.add_argument('key', type=str, required=True, 
+                     help='Enter your key as a cli arg with -d')
+
+app = Flask(__name__)
+api = Api(app)
+app.config['BUNDLE_ERRORS'] = True
+
+
+def run_ansible(state, key, hosts):
+
+    remote_user = 'ec2-user'
+    remote_file = "/home/%s/.ssh/authorized_keys" %(remote_user)
+
+    if len(hosts) == 1 :
+       hosts = hosts[0] + ','
+    else:
+       hosts = ','.join(hosts)
+       
+    cmd  = 'export ANSIBLE_HOST_KEY_CHECKING=False && '
+    cmd += 'ansible all -i %s -m lineinfile ' %(hosts)
+    cmd += " -a \"path=%s state=%s line='%s'\"" %(remote_file,state,key)
+    cmd += " -u %s"  %(remote_user)
+                
+    try:
+        os.system(cmd)
+        if state == 'present':
+            msg = "Key added to hosts - Success"
+        else:
+            msg = "Key removed from hosts - Success"
+    except:
+        if state == 'absent':
+            msg = "Unable to add key to hosts - Error - please try again"
+        else:
+            msg = "Unable to remove key from hosts - Error - please try again"
+    return msg
+
+
+class AddKeys(Resource):
+    def put(self):
+        args    = parser.parse_args()
+        key     = args['key']
+        hosts   = args['host']
+        state   = 'present'
+        add_key = run_ansible(state, key, hosts)
+        return add_key
+        
+class RemoveKeys(Resource):
+    def put(self):
+        args       = parser.parse_args()
+        key        = args['key']
+        hosts      = args['host']
+        state      = 'absent'
+        remove_key = run_ansible(state, key, hosts)
+        return remove_key
+        
+api.add_resource(AddKeys, '/add')
+api.add_resource(RemoveKeys, '/delete')
+if __name__ == '__main__':
+    app.run(debug=True) 
